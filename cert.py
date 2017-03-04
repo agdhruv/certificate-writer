35c6457e821f4ba4feb8d488b2af2b38ffaabd2a
agdhruv <dhruv_agarwal@live.com>
2017-03-05 00:50:08 +0530

    Minor comments added.

--- cert.py
+++ cert.py
@@ -14,8 +14,8 @@ txBox = slide.shapes.add_textbox(left, top, width, height)
 tf = txBox.text_frame
 
 p = tf.add_paragraph()
-student_name = ""
-student_college = ""
+student_name = "" # insert name of student here
+student_college = "" # insert name of college here
 p.text = "This is to certify that " + student_name + "\nof " + student_college + " participated\nin Vocal Solo at the inter-collegiate festival\n of Ashoka University, Banjaara 2017."
 p.font.color.rgb = RGBColor(255, 255, 255)
 p.font.size = Pt(30)
